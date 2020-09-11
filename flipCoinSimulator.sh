097343cde0460c0e0aeb840dcc54eeb978f08ed2
Rachana R <rachanarrachu28@gmail.com>
2020-09-11 21:59:52 +0530

    Added : Displaying head or tail won

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,3 +1,9 @@
 #! /bin/bash 
 echo "Welcome to Flip coin simulation program"
-
+randomCheck=$((RANDOM%2))
+if [ $randomCheck -eq 1 ]
+then
+        echo "Head won"
+else
+        echo "Tail won"
+fi
